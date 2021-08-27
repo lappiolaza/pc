28d3ee38d684d156ff047f45158bd793731362e3
Leonardo <leonardo.appiolaza@est.fi.uncoma.edu.ar>
2021-08-26 23:42:19 -0300

    ejercicio 7 a,b,c

---
+++ repaso/src/repaso/Tres.java
@@ -0,0 +1,34 @@
+package repaso;
+
+ class Tres {
+	private static int metodo(){
+		int valor=0;
+		try {
+			valor=valor +1;
+			valor=valor +1 + Integer.parseInt("W");
+			valor=valor +1;
+			System.out.println("Valor final del try: "+ valor);
+		}catch (NumberFormatException e) {
+			valor=valor +1 + Integer.parseInt("W");
+			System.out.println("Valor final del catch: "+ valor);
+		}finally {
+			valor=valor+1;
+			System.out.println("Valor final del finaly: "+ valor);
+		}
+		valor=valor +1;
+		System.out.println("Valor antes del return: "+ valor);
+		return valor;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println(metodo());
+		}catch (Exception e) {
+			System.err.println("excetion en metodo");
+			e.printStackTrace();
+		}
+
+	}
+}
